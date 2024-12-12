5de39cd540aa9cc3b4f6a223c9511007a9584b4b
Fiza Rafakat <164252341+FiZaRafakat@users.noreply.github.com>
2024-12-12 12:42:10 +0500

    12th class

---
+++ 12th class/WhileLoop.py
@@ -0,0 +1,61 @@
+# Miss Musaika
+# While loop
+
+# i = 1
+# while i <6:
+#     print(i)
+#     i += 1
+
+
+# Break Statement 
+# i = 1
+# while i <6:
+#     print(i)
+#     if i==3: 
+#         break
+#     i = i + 1
+
+
+#continue
+# i = 0
+# while i <6:
+#     i = i + 1
+#     if i==3: 
+#         continue
+#     print(i)
+
+
+# Make a loop for iterating while user enter greater than 100 number
+# a = int(input("Enter any number: "))
+# while a < 100:
+#     print(a)
+#     a = int(input("Enter any number:"))
+
+
+# Else statement 
+
+# i = 1
+# while i < 6 :
+#     print(i)
+#     i += 1
+# else :
+#     print('loop is ended')
+
+
+# Task 
+
+'''Number Mutliple of 5 and divisible by 7 (1500-2700)'''
+
+# for number in range(1500, 2701 ):
+#     if number %5==0 and number %7==0 :
+#         print(number,end='  ')
+
+'''Sum of squares of list elements'''
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# sum=0
+# for num in numbers:
+#     square = num ** 2
+#     print(square)
+#     sum = square + sum
+#     print(sum)
